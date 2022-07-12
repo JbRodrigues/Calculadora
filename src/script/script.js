f62dd6b1537d72668e1153cd046a166ea9a69070
Julio Bisaia Rodrigues <juliobisaia@hotmail.com>
2022-07-12 12:00:42 -0300

    add script

---
+++ src/script/script.js
@@ -0,0 +1,16 @@
+let tela = document.querySelector('.display')
+
+let um = documen.querySelector('#um').value
+let dois = documen.querySelector('#dois').value
+let tres = documen.querySelector('#tres').value
+let quatro = documen.querySelector('#quatro').value
+let cinco = documen.querySelector('#cinco').value
+let seis = documen.querySelector('#seis').value
+let sete = documen.querySelector('#sete').value
+let oito = documen.querySelector('#oito').value
+let nove = documen.querySelector('#nove').value
+
+let menos = documen.querySelector('#menos').value
+let mais = documen.querySelector('#mais').value
+let vezes = documen.querySelector('#vezes').value
+let dividir = documen.querySelector('#divisao').value
