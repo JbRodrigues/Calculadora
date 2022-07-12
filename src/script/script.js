00ed43c71bc8f580feeda7fca08a08a8e5ef1479
Julio Bisaia Rodrigues <juliobisaia@hotmail.com>
2022-07-12 12:41:12 -0300

    Update

--- src/script/script.js
+++ src/script/script.js
@@ -1,16 +1,23 @@
-let tela = document.querySelector('.display')
+let tela = document.getElementById('display');
 
-let um = documen.querySelector('#um').value
-let dois = documen.querySelector('#dois').value
-let tres = documen.querySelector('#tres').value
-let quatro = documen.querySelector('#quatro').value
-let cinco = documen.querySelector('#cinco').value
-let seis = documen.querySelector('#seis').value
-let sete = documen.querySelector('#sete').value
-let oito = documen.querySelector('#oito').value
-let nove = documen.querySelector('#nove').value
+let um = document.querySelector('#um')
+let dois = document.querySelector('#dois')
+let tres = document.querySelector('#tres')
+let quatro = document.querySelector('#quatro')
+let cinco = document.querySelector('#cinco')
+let seis = document.querySelector('#seis')
+let sete = document.querySelector('#sete')
+let oito = document.querySelector('#oito')
+let nove = document.querySelector('#nove')
 
-let menos = documen.querySelector('#menos').value
-let mais = documen.querySelector('#mais').value
-let vezes = documen.querySelector('#vezes').value
-let dividir = documen.querySelector('#divisao').value
+let menos = document.querySelector('#menos')
+let mais = document.querySelector('#mais')
+let vezes = document.querySelector('#vezes')
+let dividir = document.querySelector('#divisao')
+
+
+function listener(number) {
+    tela.innerHTML += number;
+}
+
+
