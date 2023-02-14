c4141957c1f2160e62e3d38c84ce37333e0cd5ea
Julio Bisaia Rodrigues <juliobisaia@hotmail.com>
2023-02-14 16:41:15 -0300

    End of project

--- src/script/script.js
+++ src/script/script.js
@@ -1,46 +1,16 @@
-let tela = document.getElementById('display');
-let limpa = document.getElementById('clear');
-
-let um = document.querySelector('#um')
-let dois = document.querySelector('#dois')
-let tres = document.querySelector('#tres')
-let quatro = document.querySelector('#quatro')
-let cinco = document.querySelector('#cinco')
-let seis = document.querySelector('#seis')
-let sete = document.querySelector('#sete')
-let oito = document.querySelector('#oito')
-let nove = document.querySelector('#nove')
-
-let menos = document.querySelector('#menos')
-let mais = document.querySelector('#mais')
-let vezes = document.querySelector('#vezes')
-let dividir = document.querySelector('#divisao')
-
-let total = 0;
-
-function listener(number) {
-
-    if (typeof number == 'number') {
-        parseInt(number)
-        tela.innerHTML += number;
-        total = tela.value;
-    } else if(typeof number == 'string') {
-        tela.innerHTML += number;
-        total = tela.value;
-        console.log(number)
-    }
-
+// This function clear all the values
+function clearScreen() {
+    document.getElementById("result").value = "";
 }
 
+// This function display values
+function display(value) {
+    document.getElementById("result").value += value;
+}
 
-
-limpa.addEventListener('click', function clear() {
-    tela.innerHTML = '';
-    total = 0;
-})
-
-
-
-function digitou(event) {
-    alert("You pressed button: " + event.button)
+// This function evaluates the expression and returns result
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p); //return the result of an expression
+    document.getElementById("result").value = q;
 }
